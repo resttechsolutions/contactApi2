f87f71922721cb885132116c9104793ff16bc108
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-09-02 16:45:58 -0400

    Service layer finished

--- src/main/java/com/resttechsolutions/contactapi2/service/implementation/AddressService.java
+++ src/main/java/com/resttechsolutions/contactapi2/service/implementation/AddressService.java
@@ -1,4 +1,95 @@
 package com.resttechsolutions.contactapi2.service.implementation;
 
-public class AddressService {
+import com.resttechsolutions.contactapi2.entity.Address;
+import com.resttechsolutions.contactapi2.repository.AddressRepository;
+import com.resttechsolutions.contactapi2.service.IService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AddressService implements IService<Address,Long> {
+
+    private Logger log = LoggerFactory.getLogger(AddressService.class);
+    private AddressRepository ar;
+
+    public AddressService(AddressRepository ar) {
+        this.ar = ar;
+    }
+
+    @Override
+    public Address create(Address address) {
+        log.info("Address create init");
+
+        try{
+            return ar.save(address);
+        } catch (Exception e){
+            log.error("The address can\'t be creadted");
+
+            e.printStackTrace();
+
+            return new Address();
+        }
+    }
+
+    @Override
+    public Address findById(Long aLong) {
+        log.info("Address findById init");
+
+        try{
+            return ar.findById(aLong).orElse(new Address());
+        } catch (Exception e){
+            log.error("The address can\'t be found");
+
+            e.printStackTrace();
+
+            return new Address();
+        }
+    }
+
+    @Override
+    public List<Address> findAll() {
+        log.info("Address findAll init");
+
+        try{
+            return ar.findAll();
+        } catch (Exception e){
+            log.error("The address list can\'t be loaded");
+
+            e.printStackTrace();
+
+            return new ArrayList<Address>();
+        }
+    }
+
+    @Override
+    public Address update(Address address) {
+        log.info("Address update init");
+
+        try{
+            return ar.save(address);
+        } catch (Exception e){
+            log.error("The address can\'t be updated");
+
+            e.printStackTrace();
+
+            return new Address();
+        }
+    }
+
+    @Override
+    public void delete(Long aLong) {
+        log.info("Address create init");
+
+        try{
+            ar.delete(ar.findById(aLong).get());
+        } catch (Exception e){
+            log.error("The address can\'t be creadted");
+
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/resttechsolutions/contactapi2/service/implementation/PhoneService.java
+++ src/main/java/com/resttechsolutions/contactapi2/service/implementation/PhoneService.java
@@ -1,4 +1,95 @@
 package com.resttechsolutions.contactapi2.service.implementation;
 
-public class PhoneService {
+import com.resttechsolutions.contactapi2.entity.Phone;
+import com.resttechsolutions.contactapi2.repository.PhoneRepository;
+import com.resttechsolutions.contactapi2.service.IService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PhoneService implements IService<Phone, Long> {
+
+    private Logger log = LoggerFactory.getLogger(Phone.class);
+    private PhoneRepository pr;
+
+    public PhoneService(PhoneRepository pr) {
+        this.pr = pr;
+    }
+
+    @Override
+    public Phone create(Phone phone) {
+        log.info("Phone create init");
+
+        try{
+            return pr.save(phone);
+        } catch (Exception e){
+            log.error("Phone can\'t be created");
+
+            e.printStackTrace();
+
+            return new Phone();
+        }
+    }
+
+    @Override
+    public Phone findById(Long aLong) {
+        log.info("Phone findById init");
+
+        try{
+            return pr.findById(aLong).orElse(new Phone());
+        } catch (Exception e){
+            log.error("Phone can\'t be found");
+
+            e.printStackTrace();
+
+            return new Phone();
+        }
+    }
+
+    @Override
+    public List<Phone> findAll() {
+        log.info("Phone findAll init");
+
+        try{
+            return pr.findAll();
+        } catch (Exception e){
+            log.error("Phone list can\'t be loaded");
+
+            e.printStackTrace();
+
+            return new ArrayList<Phone>();
+        }
+    }
+
+    @Override
+    public Phone update(Phone phone) {
+        log.info("Phone update init");
+
+        try{
+            return pr.save(phone);
+        } catch (Exception e){
+            log.error("Phone can\'t be updated");
+
+            e.printStackTrace();
+
+            return new Phone();
+        }
+    }
+
+    @Override
+    public void delete(Long aLong) {
+        log.info("Phone delete init");
+
+        try{
+            pr.delete(pr.findById(aLong).get());
+        } catch (Exception e){
+            log.error("Phone can\'t be deleted");
+
+            e.printStackTrace();
+        }
+    }
 }
