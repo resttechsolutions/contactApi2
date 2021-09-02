f7eb67d5cf2a32c64e71050b957fb0d383d3b84c
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-09-02 14:41:31 -0400

    Initial commit

---
+++ src/main/java/com/resttechsolutions/contactapi2/ContactApi2Application.java
@@ -0,0 +1,13 @@
+package com.resttechsolutions.contactapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactApi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/resttechsolutions/contactapi2/ContactApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttechsolutions.contactapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
